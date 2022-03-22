b2eb2b7d8b5ddf1e1631fd139d3d2ab2bb60ec4b
Trần Ngọc Nhật <anhchangthode22145@gmail.com>
2022-03-22 15:51:35 +0700

    Update code add tokenize and tagger object

--- nlp-toolkit/app/views/toolkit.py
+++ nlp-toolkit/app/views/toolkit.py
@@ -3,6 +3,49 @@
 import re
 from app import engine
 from flask import jsonify, request
+import os
+from .tokenizer import Tokenizer
+from .tagger import Tagger
+from .vtrie import VTrie
+import string
+
+vtrie = VTrie()
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'models', 'vocab.txt'), 'r', encoding='utf-8') as fin:
+        word = fin.read().strip()
+        if word:
+            vtrie.add_word(word)
+except:
+    pass
+
+tokenizer = Tokenizer(os.path.join(os.path.dirname(__file__), 'models', 'seggment-base.kernel'))
+postagger = Tagger(os.path.join(os.path.dirname(__file__), 'models', 'tagger-base.kernel'))
+
+def toolkit_raw_tokenize(sentence):
+    features = vtrie.extract_words(sentence)
+    for i, v in enumerate(features):
+        tmp = v.split(' ')
+        if tmp[0].lower() in ["ông","bà","anh","chị","em"]:
+            v = ' '.join(tmp[1:])
+            features[i] = v
+    tokens = set([ token for token in features if ' ' in token ])
+    tmp = [ word.replace('_',' ') for word in tokenizer.tokenize(sentence).split(' ') if '_' in word ]
+    for token in tmp:
+        v = token.split(' ')
+        if v[0].lower() in ["ông","bà","anh","chị","em"]:
+            token = ' '.join(v[1:])
+        if token not in tokens:
+            tokens.add(token)
+    tokens = sorted(tokens,key=len, reverse=True)
+    for token in tokens:
+        sentence = sentence.replace(token, token.replace(' ','_'))
+    for ch in string.punctuation:
+        if ch == ' ' or ch == '_':
+            continue
+        sentence = sentence.replace(ch, ' '+ch+' ')
+    sentence = re.sub(r'\s+',' ',sentence)
+    return sentence
 
 @engine.route("/")
 def toolkit_homepage():
@@ -26,7 +69,8 @@ def toolkit_tokenize():
         sentence = sentence.replace('/I_W', ' ')
         sentence = re.sub(r'\s+',' ', sentence)
     #  Handle tokenizer sentence and response to client
-    tokens = [ token.strip() for token in sentence.split(' ') if token.strip() ]
+    tokenized = tokenizer.tokenize(sentence)
+    tokens = [ token.strip() for token in tokenized.split(' ') if token.strip() ]
     return jsonify({
         'sentence': sentence,
         'tokenized': tokens
@@ -50,11 +94,7 @@ def toolkit_tagger():
         sentence = re.sub(r'/[A-Z][a-z]\s+',' ', sentence)
         sentence = re.sub(r'\s+',' ', sentence)
     #  Handle tagger sentence and response to client
-    tokens = [ token.strip() for token in sentence.split(' ') if token.strip() ]
-    tagged = [[], []]
-    for token in tokens:
-        tagged[0].append(token)
-        tagged[1].append('N')
+    tagged = postagger.postagging(tokenizer.tokenize(sentence))
     return jsonify({
         'sentence': sentence,
         'tagged': tagged
@@ -86,17 +126,15 @@ def toolkit_predict():
             sentence = re.sub(r'/[A-Z][a-z]\s+',' ', sentence)
         sentence = re.sub(r'\s+',' ', sentence)
 
+    tokenized = toolkit_raw_tokenize(sentence).strip()
     if datatype == 'token':
-        tokens = [ token.strip() for token in sentence.split(' ') if token.strip() ]
+        tokens = [ token.strip() for token in tokenized.split(' ') if token.strip() ]
         return jsonify({
             'sentence': sentence,
             'tokenized': tokens
         })
-    tokens = [ token.strip() for token in sentence.split(' ') if token.strip() ]
-    tagged = [[], []]
-    for token in tokens:
-        tagged[0].append(token)
-        tagged[1].append('N')
+    tokens = [ token.strip() for token in tokenized.split(' ') if token.strip() ]
+    tagged = postagger.postagging(tokenized)
     return jsonify({
         'sentence': sentence,
         'tagged': tagged
